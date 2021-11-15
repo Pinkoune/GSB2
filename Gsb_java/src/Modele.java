5123d393efa6a29dbd4c90b8dc468cd299e0f46c
Pinkoune <76524822+Pinkoune@users.noreply.github.com>
2021-11-15 08:57:41 +0100

    Premier commit

---
+++ Gsb_java/src/Modele.java
@@ -0,0 +1,68 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Modele {
+	
+	//Attributs
+	public static Connection connexion;
+	public static Statement st;
+	public static ResultSet rs;
+	private static PreparedStatement pst;
+	
+	// Methodes de connexion à la bdd
+	public static void connexionBdd() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connexion = DriverManager.getConnection("jdbc:mysql://172.16.203.212/rasio?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC", "sio", "slam");
+			//connexion = DriverManager.getConnection("jdbc:mysql://127.0.0.1/rasio?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC", "root", "");
+			st = connexion.createStatement();
+		} catch (ClassNotFoundException e) {
+			System.out.println("Le driver n'as pu être chargé");
+			//	erreur.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Erreur de la connexion à la bdd");
+			//	e.printStackTrace();
+		}
+	}
+	
+	//Méthode de la déconnexion de la bdd
+	public  static void deconnexion() {
+		try {
+			connexion.close(); // Fermeture de la connexion
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Erreur de la deconnexion");
+			e.printStackTrace();
+		} 	
+	}
+	
+	public static boolean connecter(String unLogin, String unMdp) {
+		Modele.connexionBdd();
+		boolean rep = false; 
+		int j = 0 ;
+		try {
+			pst = connexion.prepareStatement("SELECT COUNT(*) AS nb FROM compte WHERE login = ? AND mdp = ?");
+            pst.setString(1, unLogin);
+            pst.setString(2, unMdp);
+            rs = pst.executeQuery();
+			while( rs.next()) { // .next il rentre dans le tableau à la premier ligne et le parcours l ar l
+				j = rs.getInt("nb");
+			}
+			if ( j == 1 ) {
+				rep = true;
+			}
+		}catch (SQLException e) {
+			System.out.println("Erreur connexion");
+		}
+		Modele.deconnexion();
+		return rep;
+	}
+	
+	
+	
+	
+}
